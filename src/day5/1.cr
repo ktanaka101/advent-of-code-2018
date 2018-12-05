64999d8ae119b0bca9db8754590135c65b8f8b93
Kentaro Tanaka <kentanaka101@gmail.com>
2018-12-05 15:17:07 +0900

    Clearn up day5

--- src/day5/1.cr
+++ src/day5/1.cr
@@ -3,19 +3,19 @@ module Day5_1
 
   def answer(lines : Array(String)) : Int32
     str = lines.first
-
-    same_remove(str, 0).size
+    react_str(str, 0).size
   end
 
-  def same_remove(str : String, i : Int32) : String
-    return str if i == (str.size - 1)
+  def react_str(str : String, i : Int32) : String
+    return str if i >= (str.size - 1)
 
     c = str[i]
     comp_c = str[i + 1]
 
-    return same_remove(str, i + 1) if c.lowercase? && comp_c.lowercase?
-    return same_remove(str, i + 1) if c.uppercase? && comp_c.uppercase?
-    return same_remove(str, i + 1) if c.downcase != comp_c.downcase
-    return same_remove(str[0...i] + str[(i + 2)..-1], i == 0 ? 0 : i - 1)
+    return react_str(str, i + 1) if c.lowercase? && comp_c.lowercase?
+    return react_str(str, i + 1) if c.uppercase? && comp_c.uppercase?
+    return react_str(str, i + 1) if c.downcase != comp_c.downcase
+
+    react_str(str[0...i] + str[(i + 2)..-1], i == 0 ? 0 : i - 1)
   end
 end
--- src/day5/2.cr
+++ src/day5/2.cr
@@ -4,28 +4,26 @@ module Day5_2
   def answer(lines : Array(String)) : Int32
     str = lines.first
 
-    result_hs = {} of Char => Int32
-    ('a'..'z').each do |delete_c|
-      temp_str = str
-      upper_c = delete_c.upcase
-      down_c = delete_c
-      temp_str = temp_str.delete(upper_c).delete(down_c)
+    ('a'..'z').to_a.zip(('A'..'Z').to_a).reduce(Int32::MAX) do |min_size, (down_case_delete_c, upcase_delete_c)|
+      temp_str = str.delete { |c|
+        {down_case_delete_c, upcase_delete_c}.includes?(c)
+      }
 
-      result_hs[delete_c] = same_remove(temp_str, 0).size
+      size = react_str(temp_str, 0).size
+      size < min_size ? size : min_size
     end
-
-    result_hs.min_by { |_, value| value }[1]
   end
 
-  def same_remove(str : String, i : Int32) : String
-    return str if i == (str.size - 1)
+  def react_str(str : String, i : Int32) : String
+    return str if i >= (str.size - 1)
 
     c = str[i]
     comp_c = str[i + 1]
 
-    return same_remove(str, i + 1) if c.lowercase? && comp_c.lowercase?
-    return same_remove(str, i + 1) if c.uppercase? && comp_c.uppercase?
-    return same_remove(str, i + 1) if c.downcase != comp_c.downcase
-    return same_remove(str[0...i] + str[(i + 2)..-1], i == 0 ? 0 : i - 1)
+    return react_str(str, i + 1) if c.lowercase? && comp_c.lowercase?
+    return react_str(str, i + 1) if c.uppercase? && comp_c.uppercase?
+    return react_str(str, i + 1) if c.downcase != comp_c.downcase
+
+    react_str(str[0...i] + str[(i + 2)..-1], i == 0 ? 0 : i - 1)
   end
 end
